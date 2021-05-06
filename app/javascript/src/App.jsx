b4599d8ad4e6b53a1462586664b35c3f99fd0e5f
Ibrat Nawaz <ibratrocker99@gmail.com>
2021-05-06 16:44:24 +0530

    Added React.js to the project

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import { initializeLogger } from "common/logger";
+
 const App = () => {
   useEffect(() => {
     /*eslint no-undef: "off"*/
     initializeLogger();
-    logger.info("Log from js-logger");
   }, []);
 
   return (
